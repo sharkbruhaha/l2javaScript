8f90b7e79660a58174a4a006a3862fdb0a6c4941
Joseph Sharkey <shark.bruhaha@gmail.com>
2022-01-25 18:54:07 -0500

    init

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,21 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on("renderMyActorSheet", (app, html) => {
+    console.log("Modifying Actor Sheet");
+
+    // Get current Actor
+    const currentActor = app.object;
+
+    // My field
+    const myField = document.createElement("input");
+    myField.type = "text";
+    myField.id = "Ancestry";
+    myField.value = currentActor.getFlag("module-name", "flag-name");
+    myField.placeholder = "Ancestry HERE Placeholder";
+    myField.name = `flags.${"module-name"}.${"flag-name"}`;
+
+    // Add to Actor Sheet
+    html[0].querySelector("mySelector").append(myField);
+});
